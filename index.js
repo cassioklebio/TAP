f33fcadb1da6814ca58b412225d00f17193dbb37
Cássio Klebio Pereira Lins <cassiotr2009@gmail.com>
2019-02-21 21:10:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,45 +2,36 @@ const fs = require('fs');
 const archiver = require('archiver');
 const extract = require('extract-zip');
 
+
 //fornecendo a opção, caminho de destino e origem dos arquivos
+
+
+
+const tipo = process.argv[2];
+const origem = process.argv[3];
+const destino = process.argv[4];
 
 if(process.argv.length < 5) {
   console.log("Forneça do dados assim node nome do arquivo caminho1 e caminho2");
   process.exit(-1);
 }
 
-let tipo = process.argv[2];
-let origem = process.argv[3];
-let destino = process.argv[4];
-
 //Faz a compactacao do arquivo fornecido
-if(tipo === 'compactacao'){
+if(tipo == 'compactacao'){
   var saida = fs.createWriteStream(destino);
-  var compactador = archiver('zip',{zlib: {level: 9 }
-  })
+  var compactador = archiver('zip');
 
   saida.on('close',()=>{
-      console.log(compactador.pointer()+ 'bytes totais');
+      console.log(compactador.pointer()+ 'bytes totais')
   });
 
 
-  compactador.pipe(saida)
-
-  fs.readdir(origem, (err, files) =>{
-    if (err){
-      process.exit(-1)
-    }
-    files.forEach(f =>{
-        compactador.append(fs.createReadStream( origem + f ),{ name: f })
-        //  compactador.directory(origem, destino, '/')
-    });
-
-      compactador.finalize();
-  })
-
+  compactador.pipe(saida);
+  compactador.directory(origem, destino);
+  compactador.finalize();
 
 }// Faz a descompactacao do arquivo fonecido
-else if( tipo === 'descompactacao'){
+else if( tipo == 'descompactacao'){
   extract(origem,{dir: destino}, function (err) {
 
 });
